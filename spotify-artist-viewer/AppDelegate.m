28d40604201a7043504c1b14d4665c421925d116
Liz Vanderkloot <liz@lizvdk.com>
2016-01-11 15:19:20 -0500

    Remove storyboard, start from nib

--- spotify-artist-viewer/AppDelegate.m
+++ spotify-artist-viewer/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -15,7 +16,13 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+	ViewController *viewController = [ViewController new];
+	UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:viewController];
+	
+	CGRect screenFrame = [UIScreen mainScreen].bounds;
+	self.window = [[UIWindow alloc] initWithFrame:screenFrame];
+	self.window.rootViewController = nav;
+	[self.window makeKeyAndVisible];
     return YES;
 }
 
